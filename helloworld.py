2c81ccb9576dcdf0725c850a6f7d1c30842ed390
yjsyks <soledad_yks@163.com>
2021-07-19 10:45:39 +0800

    master

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+'''
+Author: your name
+Date: 2021-07-19 10:44:24
+LastEditTime: 2021-07-19 10:44:35
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \test\helloworld.py
+'''
+print('hello world')
