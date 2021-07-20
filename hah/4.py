cc2b14c65a4a40704b592dd5947f83c76e923eca
yjsyks <soledad_yks@163.com>
2021-07-20 20:17:02 +0800

    master

--- hah/4.py
+++ hah/4.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-07-20 18:32:30
-LastEditTime: 2021-07-20 20:12:44
+LastEditTime: 2021-07-20 20:15:40
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \Github\test\hah\4.py
@@ -9,3 +9,4 @@ FilePath: \Github\test\hah\4.py
 a=1
 b=2
 print(a,b)
+print('123')
