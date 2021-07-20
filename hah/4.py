bdb7e8d5f64cf56377c70a7a2400763d6fa65bdf
yjsyks <soledad_yks@163.com>
2021-07-20 20:13:41 +0800

    master

--- hah/4.py
+++ hah/4.py
@@ -1,2 +1,11 @@
+'''
+Author: your name
+Date: 2021-07-20 18:32:30
+LastEditTime: 2021-07-20 20:12:44
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \Github\test\hah\4.py
+'''
 a=1
 b=2
+print(a,b)
