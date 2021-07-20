5bf09423aac0504afaebfd4a5b6a265f8d8b7ad5
yjsyks <soledad_yks@163.com>
2021-07-20 20:47:24 +0800

    master

--- hah/4.py
+++ hah/4.py
@@ -1,7 +1,7 @@
 '''
-Author: your name
+Author: yks
 Date: 2021-07-20 18:32:30
-LastEditTime: 2021-07-20 20:15:40
+LastEditTime: 2021-07-20 20:46:54
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \Github\test\hah\4.py
